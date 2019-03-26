a71e190ec7716cca9de0654c40e0f6e8d80abe34
Camilo Romero <camilo1rp@gmail.com>
2019-03-26 15:43:14 -0500

    Initial commit

---
+++ store.py
@@ -0,0 +1,28 @@
+"""The program should be capable of adding client, items and orders. Users should be able to be linked
+with orders and orders should include items and quantities. The programs should keep track of
+ number of in store and dispatched items and orders"""
+
+#Class Client
+class Client():
+    #Contructor
+    def __init__(self, name, phone, email="noemail@null.com"):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.orders = {}
+
+    def set_email(self, new_email):
+        self.email = new_email
+
+    def set_phone(self, new_phone):
+        self.phone = new_phone
+
+
+
+
+
+
+
+    #
+
+
